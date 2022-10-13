e385bf8b7e7f541b7ab663b01c5a6bb3dac3f02a
mannyhappenings <mannyhappenings@gmail.com>
2022-10-13 11:40:51 +0530

    fix function format

--- netlify/functions/discord-init.js
+++ netlify/functions/discord-init.js
@@ -4,23 +4,29 @@ const colors = require("colors"); //this Package is used, to change the colors o
 const fs = require("fs");
 require('dotenv/config'); //this package is for reading files and getting their inputs
 
-//Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
-const client = new Discord.Client({
-    messageCacheLifetime: 60,
-    fetchAllMembers: false,
-    messageCacheMaxSize: 10,
-    restTimeOffset: 0,
-    restWsBridgetimeout: 100,
-    disableEveryone: true,
-    partials: ['MESSAGE', 'CHANNEL']
-});
+const handler = () => {
 
-//Loading files, with the client variable like Command Handler, Event Handler, ...
-require('../../events/guild/message')(client);
+    //Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
+    const client = new Discord.Client({
+        messageCacheLifetime: 60,
+        fetchAllMembers: false,
+        messageCacheMaxSize: 10,
+        restTimeOffset: 0,
+        restWsBridgetimeout: 100,
+        disableEveryone: true,
+        partials: ['MESSAGE', 'CHANNEL']
+    });
 
-const token = process.env.BOT_TOKEN ?? require('../../botconfig/config.json').token
+    //Loading files, with the client variable like Command Handler, Event Handler, ...
+    require('../../events/guild/message')(client);
 
-//login into the bot
-client.login(token);
+    const token = process.env.BOT_TOKEN ?? require('../../botconfig/config.json').token
 
-/** Template by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template */
+    //login into the bot
+    client.login(token);
+}
+
+
+export {
+    handler
+};
